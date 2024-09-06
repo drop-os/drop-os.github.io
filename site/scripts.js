1e03060beba3a368529ccd2c1815c166614c55a8
avery-jpg <jxstmeliving@gmail.com>
2024-09-05 23:09:42 -0700

    Update scripts.js

--- site/scripts.js
+++ site/scripts.js
@@ -1,1 +1,32 @@
 
+function toggleStartMenu() {
+    const menu = document.getElementById('start-menu');
+    menu.classList.toggle('hidden');
+}
+
+// Basic window dragging functionality
+let draggingElement = null;
+let offsetX, offsetY;
+
+document.addEventListener('mousedown', function(e) {
+    if (e.target.classList.contains('title-bar')) {
+        draggingElement = e.target.parentElement;
+        offsetX = e.clientX - draggingElement.getBoundingClientRect().left;
+        offsetY = e.clientY - draggingElement.getBoundingClientRect().top;
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+    }
+});
+
+function onMouseMove(e) {
+    if (draggingElement) {
+        draggingElement.style.left = e.clientX - offsetX + 'px';
+        draggingElement.style.top = e.clientY - offsetY + 'px';
+    }
+}
+
+function onMouseUp() {
+    draggingElement = null;
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+}
